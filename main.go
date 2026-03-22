c378e59569081a421fa8d63861db6f433eac64fd
Amin Chawki <amin.chawki@devmino.cloud>
2026-03-22 15:28:39 +0100

    Harden runtime and deployment setup

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+
 	"github.com/achawki/webhook-receiver/cmd/receiver"
 )
 
 func main() {
 	server := receiver.Setup()
-	server.Start()
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := server.Run(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
